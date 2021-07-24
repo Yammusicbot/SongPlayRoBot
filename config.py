9c348aa5647735bea6c9e982e1a9184f14e90031
Yammusicbot <87897448+Yammusicbot@users.noreply.github.com>
2021-07-24 15:48:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "7419154"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "feb56a4e3bc369105703ae534c840720" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1854364829:AAEEmCK-SyX6Nlyx1J-_-RyfHW-BwCa6STc" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
